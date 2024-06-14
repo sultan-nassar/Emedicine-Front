c3178c64294542659f632350798b7904a6bef911
sultan-nassar <sultannassar11@gmail.com>
2024-06-14 19:53:11 +0300

    new footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,52 +1,4 @@
 
-// import React from 'react';
-// import './Footer.css'; // Import your CSS file
-
-// const Footer = () => {
-//   const currentYear = new Date().getFullYear();
-
-//   return (
-//     <footer className="footer">
-//       <div className="container py-3">
-//         <div className="row">
-//           <div className="col-md-4 text-white">
-//             <h5>Pages</h5>
-//             <ul className="list-unstyled">
-//               <li><a href="/" className="text-white">Home</a></li>
-//               <li><a href="/about" className="text-white">About Us</a></li>
-//               <li><a href="/contact" className="text-white">Contact Us</a></li>
-//             </ul>
-//           </div>
-//           <div className="col-md-4 text-white text-center">
-//             <h5>Follow Us</h5>
-//             <ul className="list-unstyled">
-//               <li><a href="https://www.facebook.com" target='_blank' rel="noopener noreferrer" className="text-white mx-2"><i className="fab fa-facebook-f"></i></a>&nbsp;Facebook</li>
-//               <li><a href="https://www.youtube.com" target='_blank' rel="noopener noreferrer" className="text-white mx-2"><i className="fab fa-youtube"></i></a>&nbsp;Youtube </li>
-//               <li><a href="https://www.whatsapp.com" target='_blank' rel="noopener noreferrer" className="text-white mx-2"><i className="fab fa-whatsapp"></i></a>&nbsp;Whatsapp</li>
-//             </ul>
-//           </div>
-//           <div className="col-md-4 text-white">
-//             <h5>About</h5>
-//             <p className="mb-0">Medicine E-commerce is your go-to online pharmacy providing a wide range of medicines and healthcare products.</p>
-//           </div>
-//         </div>
-//         <div className="row mt-3">
-//           <div className="col-md-12 text-right">
-//             <span className="text-white">© {currentYear} EMedicine E-commerce. All rights reserved.</span>
-//           </div>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-
-
-
-
-
 
 import React from 'react';
 import './Footer.css'; // Import your CSS file
@@ -98,3 +50,6 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+
